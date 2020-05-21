4bab7f452d31c71af09ed923fff88964555ba61b
victor muia <victor.muia@clio.com>
2020-05-21 15:25:41 +0100

    Fix layout

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -2,12 +2,13 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
 
-gulp.task('build', shell.task(['jekyll build --watch']));
+gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
 
 gulp.task('serve',  () => {
   browserSync.init({server: {baseDir: '_site/'}});
 
   gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+
 });
 
 gulp.task('default', gulp.parallel('build', 'serve'))
